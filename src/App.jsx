717f705e8b8aa3ecc3dcc2d341a4cf638fca4f4f
huslee <Xvclee2551@gmail.com>
2023-01-26 14:58:03 +0800

    rout ynzlav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import Home from './pages/Home'
-import { Route } from 'react-router-dom'
+import Home from './pages/Home/index'
+import { Route, Routes } from 'react-router-dom'
 import Blog from './pages/Blog'
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
-      <Route path="/" component={Home}/>
-      <Route path='/blog/:id' component={Blog}/>
+    <div className='container'>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path='/blog/:id' element={<Blog/>}/>
+      </Routes>
     </div>
   )
 }
