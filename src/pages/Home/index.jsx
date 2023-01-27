44bac59f77f3bf9acf436bfa00206b19e99e5182
huslee <Xvclee2551@gmail.com>
2023-01-27 13:08:04 +0800

    gfd

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      Page 
+      {/* Page Header */}
+
+      {/* Search Bar */}
+
+      {/* Blog list & Eempty List */}
+      
     </div>
   )
 }
