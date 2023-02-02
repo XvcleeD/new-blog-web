7ddb3bd4ace49e1160616cb2b5d3850db9da5798
XvcleeD <Xvclee255@gmail.com>
2023-02-02 18:40:20 +0800

    gdsfg

--- src/components/common/EmptyList/index.jsx
+++ src/components/common/EmptyList/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './style.css'
 
 export const EmptyList = () => {
   return (
     <div className="emptyList-wrap">
-      <img src="/assets/imgages/13525-empty.gif" alt="empty" />
+      <img src="/assets/images/13525-empty.gif" alt="wae4g3rwerempty" />
     </div>
   );
 };
